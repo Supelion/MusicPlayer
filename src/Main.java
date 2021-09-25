d243da3c19f635f4c6a8421191d7158ff872edf4
Supelion <mutayyabtariq9@gmail.com>
2021-09-25 20:49:20 +0300

    I should be studying for my test.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
-import javax.swing.JOptionPane;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 // The everything
 public class Main {
@@ -17,24 +21,35 @@ public class Main {
         clip.open(audio);
         clip.start();
 
-        String response = "";
+        JButton start = new JButton();
+        start.setBounds(10, 10, 150, 50);
+        start.setText("Start / Resume");
+        start.setFocusable(false);
+        start.setBackground(new Color(255, 255, 255));
+        start.setForeground(new Color(25, 20, 20));
+        start.addActionListener(e -> clip.start());
 
-        // As long as the response is not q, do this
-        while (!response.equals("q")) {
-            // Giving the user a list of options to choose from, because the can, unlike the people of china
-            response = JOptionPane.showInputDialog(null, "R = Resume / Play\nP = Pause\nQ = Quit");
-            response = response.toLowerCase();
+        JButton pause = new JButton();
+        pause.setBounds(300, 10, 150, 50);
+        pause.setText("Pause");
+        pause.setFocusable(false);
+        pause.setForeground(new Color(25, 20, 20));
+        pause.setBackground(new Color(255, 255, 255));
+        pause.addActionListener(e -> clip.stop());
 
-            switch (response) {
-                case "r": clip.start();
-                break;
-                case "p": clip.stop();
-                break;
-                case "q": clip.close();
-                break;
-                default: System.out.println("Invalid Response!");
-            }
-        }
+        ImageIcon logo = new ImageIcon("./images/logo.png");
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(500, 160);
+        frame.setVisible(true);
+        frame.setTitle("Supelion's Music Player");
+        frame.setResizable(false);
+        frame.setIconImage(logo.getImage());
+        frame.getContentPane().setBackground(new Color(30, 215, 96));
+        frame.add(pause);
+        frame.add(start);
 
         System.out.println("Exiting...");
     }
