a4b8684f692058f0b498d26f68860fb1d8fff0d4
Supelion <83178953+Supelion@users.noreply.github.com>
2021-10-01 10:27:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ except:
     os.system("pip3 install pydub")
 
 source = input("Input the name of the .mp3 / .wav file you would like to play (excluding .mp3): ")
+source += ".mp3"
 
 sound = AudioSegment.from_mp3(source)
 sound.export("./src/music/audio.wav", format = "wav")
